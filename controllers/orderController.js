3874bdd8f702c180b85275b3a687f442f67fbb86
Grace Callanta <callantag@gmail.com>
2021-05-19 12:10:26 +0800

    Edit connection string

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,26 +1,50 @@
-const { Order , OrderProduct }  = require("./../models/Order");
+// const { Order , OrderProduct }  = require("./../models/Order");
+const Order  = require("./../models/Order");
 const Product = require("./../models/Product");
 
 module.exports.createOrder = (req,res) => {
-	const orderProduct = new OrderProduct ({
-		productId : req.body.productId,
-		quantity : req.body.quantity
+	Order.create({
+		userId: req.user.id,
+		productId: req.body.productId,
+		quantity: req.body.quantity
 	})
-	const order = new Order ({
-		userId : req.user.id,
-		products : orderProduct
+	.then(()=> {
+		return Order.findOne({userId: req.user.id})
 	})
-	
-	orderProduct.save()
-	order.save()
-	Product.findById(req.body.productId)
-	.then(product => res.send(product.price))
-	.then (order => {
-		Order.totalAmount +=product.price
-		Order.save()
-	})
+	.then((order)=>{
+		Product.findById(req.body.productId)
+		.then(product => {
+			let subtotalValue = product.price*req.body.quantity
+			order.totalAmount +=subtotalValue
+			order.products.push({
+				productId: req.body.productId,
+				quantity: req.body.quantity,
+				subtotal: subtotalValue
+			})
+			order.save()
+			.then((updatedOrder)=>{
+				res.send(updatedOrder)
+			})
+		})
+	}, {new:true})
 	.catch(err => res.send(err))
-};
+}
+
+
+// module.exports.createOrder = (req,res) => {
+// 	const orderProduct = new OrderProduct ({
+// 		productId : req.body.productId,
+// 		quantity : req.body.quantity
+// 	})
+// 	const order = new Order ({
+// 		userId : req.user.id,
+// 		products : orderProduct
+// 	})
+// 	orderProduct.save()
+// 	order.save()
+// 	.then(order => res.send(order))
+// 	.catch(err => res.send(err))
+// };
 
 
 module.exports.getOrders = (req,res) => {
@@ -35,43 +59,6 @@ module.exports.getAllOrders = (req,res) => {
 	.then( orders => res.send( orders ))
 	.catch( err => res.send( err ))
 }
-
-// Order.update(
-	//     {userId: req.user.id, "productId": req.body.productId},
-	//     {$inc: {"products.$.quantity": Order.quantity}}
-	// )
-// const updatedtotal = OrderProduct.aggregate([
-//   {
-//     $group: {
-//       _id:null,
-//       totalAmount: {
-//         $sum:"$subtotal"
-//       }
-//     }
-//   }
-// ])
-
-//totalAmount = {$sum:"$subtotal"}
-//quantity = count: {$sum: 1}
-// subtotal = Product.price * OrderProduct.quantity
-
-// Middleware that increases the quantity in product
-// exports.increaseQuantity = (req, res, next) => {
-// 	let bulkOps = req.body.order.products.map((item) => {
-// 		return {
-// 			updateOne: {
-// 				filter: { _id: item._id },
-// 				update: { $inc: { quantity: +item.quantity } }
-// 			}
-// 		};
-// 	});
-// 	Product.bulkWrite(bulkOps, {}, (error, products) => {
-// 		if (error) {
-// 			return res.send('Could not update product quantity');
-// 		}
-// 		next();
-// 	});
-// };
 
 
 // Non-admin User checkout (Create Order)
--- models/Order.js
+++ models/Order.js
@@ -1,36 +1,66 @@
-const mongoose = require("mongoose");
-const User = require("./User");
-const Product = require("./Product");
+const mongoose = require('mongoose');
 
-const OrderProductSchema = new mongoose.Schema ({
-	productId :  {
-		type : mongoose.Schema.Types.ObjectId,
-		ref: "Product",
-		required: true
-	},
-	// unitPrice : { type : Number , default : 0 },
-	quantity :  { type : Number , required: true }
-	// subtotal :  { type : Number , default : 1 }
-})
+const orderSchema = new mongoose.Schema({
+    // structure
+    totalAmount: {
+        type: Number,
+        default: 0
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    products: [
+        {
+            productId: {
+                type: String,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            subtotal: {
+                type: Number
+            }
+        }
+    ]
+}, { timestamps: { createdAt: 'purchasedOn'}});
 
-const OrderProduct = mongoose.model( "OrderProduct" , OrderProductSchema );
+module.exports = mongoose.model('Order', orderSchema);
+
+// const mongoose = require("mongoose");
+// const User = require("./User");
+// const Product = require("./Product");
+
+// const OrderProductSchema = new mongoose.Schema ({
+// 	productId :  {
+// 		type : mongoose.Schema.Types.ObjectId,
+// 		ref: "Product",
+// 		required: true
+// 	},
+// 	quantity :  { type : Number , required: true }
+// 	// subtotal :  { type : Number , default : 0 }
+// })
+
+// const OrderProduct = mongoose.model( "OrderProduct" , OrderProductSchema );
 
 
-const OrderSchema = new mongoose.Schema({
+// const OrderSchema = new mongoose.Schema({
 	
-	// totalAmount : { type : Number , default : 0 }, 
-	userId : {
-		type : mongoose.Schema.Types.ObjectId,
-		ref: "User",
-		required: true
-	},
-	products : [OrderProductSchema]
-}, {timestamps:{createdAt: "purchasedOn"}});
+// 	// totalAmount : { type : Number , default : 0 }, 
+// 	userId : {
+// 		type : mongoose.Schema.Types.ObjectId,
+// 		ref: "User",
+// 		required: true
+// 	},
+// 	products : [OrderProductSchema]
+// }, {timestamps:{createdAt: "purchasedOn"}});
 
-const Order = mongoose.model( "Order" , OrderSchema )
+// const Order = mongoose.model( "Order" , OrderSchema )
 
 
-module.exports = { OrderProduct , Order }
+// module.exports = { OrderProduct , Order }
 
 
 
@@ -47,3 +77,4 @@ module.exports = { OrderProduct , Order }
 //   }
 
 // ]
+
